7cdc5dcf0fbb304aa7237c2bc9e2a1689e47ec27
zoovely <yon041128@naver.com>
2023-08-18 16:32:53 +0900

    [CHORE] : finish eslint, prettier setting

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,14 @@
 import '../styles/App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <h1>
-        Hello world!
-      </h1>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1 className="text-3xl font-bold underline">Hello world!</h1>
+				<h1>Hello world!</h1>
+			</header>
+		</div>
+	);
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,5 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './components/App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-    <App />
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(<App />);
