2da733ab1379efd382ceca710036afd7a441399e
lamPolar <lovast7@gmail.com>
2023-08-23 16:01:59 +0900

    [STYLE] :  변수이름 변경 & 색 변경 seat component able/disable 색 변경 & shadow 추가 & star->starred 로 상태 이름 변경

--- src/pages/CheckSeat/views/Seat.tsx
+++ src/pages/CheckSeat/views/Seat.tsx
@@ -5,37 +5,33 @@ interface SeatProps {
 	seatStatus: string;
 	seatUser?: string;
 	logoutTime?: number;
-	isStar: boolean;
+	isStarred: boolean;
 	useDay?: number;
 }
 
-function Seat({ seatLocation, seatStatus, seatUser, logoutTime, isStar, useDay }: SeatProps) {
-	let isAvaliable = false;
-	if (seatStatus === '자리 사용 가능' || seatStatus === '로그아웃') isAvaliable = true;
+function Seat({ seatLocation, seatStatus, seatUser, logoutTime, isStarred, useDay }: SeatProps) {
+	let isAvailable = false;
+	if (seatStatus === '사용 가능' || seatStatus === '로그아웃') isAvailable = true;
+	let seatState = '';
+	if (!isAvailable) seatState = `${seatUser}님이 자리를 사용중입니다`;
+	else if (logoutTime !== -1) seatState = `${logoutTime}분 전 ${seatStatus}`;
+	else seatState = `${seatStatus}한 자리입니다`;
+
 	return (
 		<div
 			id="Seat"
-			className={
-				isAvaliable
-					? 'bg-nomad-green text-nomad-sand rounded-3xl text-md w-5/6 pt-2.5 pl-5 pr-5 pb-2.5'
-					: 'bg-nomad-disable bg-opacity-50 text-gray-700 rounded-3xl text-md w-5/6 pt-2.5 pl-5 pr-5 pb-2.5 font-nexonLight'
-			}
+			className={`shadow-full shadow-zinc-900/10 rounded-3xl text-md w-5/6 pt-2.5 pl-5 pr-5 pb-2.5 text-black
+				${isAvailable ? 'bg-white' : 'bg-zinc-100 '}`}
 		>
-			{!isStar ? (
-				<div className="grid grid-cols-4">
+			{!isStarred ? (
+				<div className="grid grid-cols-4 items-center">
 					<div>{seatLocation}</div>
-					<div className={`col-start-2 col-end-3 ${isAvaliable ? 'text-white' : ' text-black'}`}>{useDay}일 전</div>
+					<div className="col-start-2 col-end-3 font-nexonLight text-xs text-gray-500">{useDay}일 전</div>
 				</div>
 			) : (
 				<div>{seatLocation}</div>
 			)}
-			{!isAvaliable && <div>{seatUser}님이 자리를 사용중입니다</div>}
-			{isAvaliable && logoutTime !== -1 && (
-				<div>
-					{logoutTime}분 전 {seatStatus}
-				</div>
-			)}
-			{isAvaliable && logoutTime === -1 && <div> {seatStatus} </div>}
+			<div className="font-nexonLight text-sm">{seatState}</div>
 		</div>
 	);
 }
