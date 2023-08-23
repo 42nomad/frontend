339f559f6bc2d31b348e54aa64acab79259b415d
lamPolar <lovast7@gmail.com>
2023-08-23 16:01:59 +0900

    [STYLE] :  변수이름 변경 & 색 변경 seat component able/disable 색 변경 & shadow 추가 & star->starred 로 상태 이름 변경

--- src/pages/CheckSeat/views/CheckSeat.tsx
+++ src/pages/CheckSeat/views/CheckSeat.tsx
@@ -4,38 +4,38 @@ import SeatButton from './SeatButton';
 import Seat from './Seat';
 
 function CheckSeat() {
-	const [isStar, setIsStar] = useState(true);
+	const [isStarred, setIsStarred] = useState(true);
 	return (
-		<>
+		<div>
 			<SideBar />
-			<div id="CheckSeat" className="bg-nomad-sand flex flex-col min-h-full">
+			<div id="CheckSeat" className="bg-nomad-sand flex flex-col min-h-screen">
 				<div id="ButtonHeader" className="flex space-x-1.5 fixed w-full max-w-max-wid pt-20 pl-9 pb-2 bg-nomad-sand">
-					<SeatButton buttonName="즐겨찾기" star isStar={isStar} setIsStar={setIsStar} />
-					<SeatButton buttonName="히스토리" star={false} isStar={isStar} setIsStar={setIsStar} />
+					<SeatButton buttonName="즐겨찾기" starred isStarred={isStarred} setIsStarred={setIsStarred} />
+					<SeatButton buttonName="히스토리" starred={false} isStarred={isStarred} setIsStarred={setIsStarred} />
 				</div>
-				{!isStar && (
-					<div className="flex flex-col mt-28 justify-center items-center space-y-2 pb-10 ">
-						<Seat seatLocation="C1R1S1" seatStatus="Not" seatUser="heeskim" isStar={isStar} useDay={0} />
-						<Seat seatLocation="C1R1S2" seatStatus="로그아웃" logoutTime={1} isStar={isStar} useDay={0} />
-						<Seat seatLocation="C1R1S3" seatStatus="Not" seatUser="heeskim" isStar={isStar} useDay={1} />
-						<Seat seatLocation="C1R1S4" seatStatus="Not" seatUser="sojoo" isStar={isStar} useDay={3} />
-						<Seat seatLocation="C1R1S5" seatStatus="Not" seatUser="hyunjcho" isStar={isStar} useDay={5} />
+				{!isStarred && (
+					<div className="flex flex-col mt-32 justify-center items-center space-y-2 pb-10 ">
+						<Seat seatLocation="C1R1S1" seatStatus="Not" seatUser="heeskim" isStarred={isStarred} useDay={0} />
+						<Seat seatLocation="C1R1S2" seatStatus="로그아웃" logoutTime={1} isStarred={isStarred} useDay={0} />
+						<Seat seatLocation="C1R1S3" seatStatus="Not" seatUser="heeskim" isStarred={isStarred} useDay={1} />
+						<Seat seatLocation="C1R1S4" seatStatus="Not" seatUser="sojoo" isStarred={isStarred} useDay={3} />
+						<Seat seatLocation="C1R1S5" seatStatus="Not" seatUser="hyunjcho" isStarred={isStarred} useDay={5} />
 					</div>
 				)}
-				{isStar && (
-					<div className="flex flex-col mt-28 justify-center items-center space-y-2 pb-10">
-						<Seat seatLocation="C1R1S1" seatStatus="Not" seatUser="heeskim" isStar={isStar} />
-						<Seat seatLocation="C1R1S2" seatStatus="로그아웃" logoutTime={1} isStar={isStar} />
-						<Seat seatLocation="C1R1S3" seatStatus="Not" seatUser="heeskim" isStar={isStar} />
-						<Seat seatLocation="C1R1S4" seatStatus="Not" seatUser="sojoo" isStar={isStar} />
-						<Seat seatLocation="C1R1S5" seatStatus="Not" seatUser="hyunjcho" isStar={isStar} />
-						<Seat seatLocation="C1R1S1" seatStatus="로그아웃" logoutTime={1} isStar={isStar} />
-						<Seat seatLocation="C1R1S1" seatStatus="로그아웃" logoutTime={33} isStar={isStar} />
-						<Seat seatLocation="C1R1S1" seatStatus="로그아웃" logoutTime={1} isStar={isStar} />
-						<Seat seatLocation="C1R1S1" seatStatus="자리 사용 가능" isStar={isStar} />
+				{isStarred && (
+					<div className="flex flex-col mt-32 justify-center items-center space-y-2 pb-10">
+						<Seat seatLocation="C1R1S1" seatStatus="Not" seatUser="heeskim" isStarred={isStarred} />
+						<Seat seatLocation="C1R1S2" seatStatus="로그아웃" logoutTime={1} isStarred={isStarred} />
+						<Seat seatLocation="C1R1S3" seatStatus="Not" seatUser="heeskim" isStarred={isStarred} />
+						<Seat seatLocation="C1R1S4" seatStatus="Not" seatUser="sojoo" isStarred={isStarred} />
+						<Seat seatLocation="C1R1S5" seatStatus="Not" seatUser="hyunjcho" isStarred={isStarred} />
+						<Seat seatLocation="C1R1S1" seatStatus="로그아웃" logoutTime={1} isStarred={isStarred} />
+						<Seat seatLocation="C1R1S1" seatStatus="로그아웃" logoutTime={33} isStarred={isStarred} />
+						<Seat seatLocation="C1R1S1" seatStatus="로그아웃" logoutTime={1} isStarred={isStarred} />
+						<Seat seatLocation="C1R1S1" seatStatus="사용 가능" isStarred={isStarred} />
 						<div
 							id="Seat"
-							className="bg-nomad-disable bg-opacity-50 text-gray-500 rounded-3xl text-md w-5/6 h-14 pt-3 pl-5 pr-5 pb-3 flex justify-center items-center"
+							className="bg-nomad-green text-nomad-sand  shadow-sm shadow-zinc-900/5 rounded-3xl text-md w-5/6 h-14 pt-3 pl-5 pr-5 pb-3 flex justify-center items-center"
 						>
 							<button type="button">
 								<svg
@@ -57,7 +57,7 @@ function CheckSeat() {
 					</div>
 				)}
 			</div>
-		</>
+		</div>
 	);
 }
 
