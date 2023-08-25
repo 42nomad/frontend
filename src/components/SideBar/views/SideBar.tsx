5b318538abf9241aa0d59a9365e1b32373ca4eb9
lamPolar <lovast7@gmail.com>
2023-08-25 18:40:39 +0900

    [STYLE] : button icon들을 heroicon import로 변경 각 menu에 imogi 추가

--- src/components/SideBar/views/SideBar.tsx
+++ src/components/SideBar/views/SideBar.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Bars3Icon } from '@heroicons/react/24/outline';
 import SideBarContent from './SideBarContent';
 
 function SideBar() {
@@ -10,29 +11,18 @@ function SideBar() {
 			<div id="Header" className="z-40 w-full max-w-max-wid bg-nomad-sand fixed">
 				<div className="flex justify-between">
 					<Link to="/" className="h-15 mt-4 ml-4">
-						<div id="Logo" className="font-fugazRegular text-2xl text-nomad-green">
+						<div id="Logo" className="font-fugazRegular text-2xl text-nomad-green cursor-pointer">
 							42NOMAD
 						</div>
 					</Link>
 					{!isOpen && (
 						<div id="HamburgerButton" className="z-50 bg-transparent">
-							<button
-								type="button"
-								className="items-center z-50 transition-transform transform hover:scale-110 mt-2 mr-4"
+							<Bars3Icon
+								className="items-center z-50 transition-transform transform hover:scale-110 mt-2 mr-4 w-10 h-10 stroke-nomad-green cursor-pointer"
 								onClick={() => {
 									setIsOpen(!isOpen);
 								}}
-							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									strokeWidth={1.5}
-									className="w-10 h-10 z-50 stroke-nomad-green"
-								>
-									<path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-								</svg>
-							</button>
+							/>
 						</div>
 					)}
 				</div>
