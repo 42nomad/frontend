f49f20d8ed0be53cacf14f265c1b2091bbb50300
lamPolar <lovast7@gmail.com>
2023-08-25 18:43:53 +0900

    [SYTLE] : 각 맵에 divide-x, border 추가

--- src/pages/MeetingRoom/views/C3Map.tsx
+++ src/pages/MeetingRoom/views/C3Map.tsx
@@ -4,9 +4,9 @@ function C3Map() {
 	return (
 		<div
 			id="MeetingMap"
-			className="grid grid-cols-5 grid-rows-12 h-2/3 w-2/3 mt-5 divide-meeting-disable border-meeting-disable divide-y-2 border-r-2"
+			className="grid grid-cols-5 grid-rows-12 h-2/3 w-2/3 mt-5 divide-black divide-y-2 divide-x-2 border-black border-b-2 border-r-2"
 		>
-			<div className="col-span-2 row-span-full flex items-center justify-center bg-meeting-disable text-nomad-sand">
+			<div className="col-span-2 row-span-full flex items-center justify-center bg-meeting-disable text-nomad-sand border-black border-t-2 border-l-2">
 				C3 Cluster
 			</div>
 			<div className="col-span-3 flex justify-center items-center bg-meeting-disable text-nomad-sand">사물함</div>
