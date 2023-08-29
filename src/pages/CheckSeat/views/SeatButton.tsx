ac69e366d4ef011ecaf5db0ddc1f36733a896df7
lamPolar <lovast7@gmail.com>
2023-08-29 18:31:13 +0900

    [REFACTOR] : 뷰 순서 정렬 & tab -> currentTab으로 상태명 변경

--- src/pages/CheckSeat/views/SeatButton.tsx
+++ src/pages/CheckSeat/views/SeatButton.tsx
@@ -2,20 +2,20 @@ import React from 'react';
 
 interface SeatButtonProps {
 	buttonName: string;
-	tab: number;
-	setTab: React.Dispatch<React.SetStateAction<number>>;
+	currentTab: number;
+	setCurrentTab: React.Dispatch<React.SetStateAction<number>>;
 	tabNumber: number;
 }
 
-function SeatButton({ buttonName, tab, tabNumber, setTab }: SeatButtonProps) {
+function SeatButton({ buttonName, currentTab, tabNumber, setCurrentTab }: SeatButtonProps) {
 	return (
 		<button
 			type="button"
 			onClick={() => {
-				setTab(tabNumber);
+				setCurrentTab(tabNumber);
 			}}
 			className={`border-collapse rounded-2xl w-20 h-8 text-md shadow-full shadow-zinc-900/10
-				${tab === tabNumber ? 'bg-nomad-green text-nomad-sand' : 'bg-white text-gray-700 font-nexonLight'}`}
+				${currentTab === tabNumber ? 'bg-nomad-green text-nomad-sand' : 'bg-white text-gray-700 font-nexonLight'}`}
 		>
 			{buttonName}
 		</button>
