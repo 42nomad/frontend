7b9588289fa4aa2696104a4f32dfcda961b3758f
lamPolar <lovast7@gmail.com>
2023-08-31 13:42:15 +0900

    [FEAT] : AbleRoom을 api에서 받은 정보로 렌더링 하도록 변경

--- src/pages/MeetingRoom/views/C5Map.tsx
+++ src/pages/MeetingRoom/views/C5Map.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { mapBorder, leftTopBorder } from './MapStyle';
 import DisableRoom from './DisableRoom';
 import AbleRoom from './AbleRoom';
+import GetMapInfo from '../logics/GetMapInfo';
 
 function C5Map() {
+	const mapInfo = GetMapInfo('C5');
+
 	return (
 		<div id="MeetingMap" className="grid grid-cols-3 grid-rows-10 h-2/3 w-2/3 mt-5" css={mapBorder}>
 			<div className="col-span-1 row-span-full" css={leftTopBorder}>
@@ -16,7 +19,7 @@ function C5Map() {
 				<DisableRoom roomName="사물함" />
 			</div>
 			<div className="col-span-2 row-span-3">
-				<AbleRoom roomName="TABLE" isAvailable noti={false} />
+				<AbleRoom mapInfo={mapInfo} cluster="C5" roomName="TABLE" />
 			</div>
 			<div className="col-span-2">
 				<DisableRoom roomName="사물함" />
