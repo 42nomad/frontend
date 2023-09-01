2fb1892c34f9230f2b94862f8122523bdd113b2a
zoovely <yon041128@naver.com>
2023-09-01 14:45:00 +0900

    [RENAME] : delete button onClick 함수 분리

--- src/pages/MyPage/views/StarredLine.tsx
+++ src/pages/MyPage/views/StarredLine.tsx
@@ -1,30 +1,7 @@
 import React from "react";
 import { XCircleIcon } from "@heroicons/react/24/outline";
-import Swal from "sweetalert2";
 import MyPageStarredData from "../../../interfaces/MyPageStarredData";
-import nomadAxios from "../../../utils/nomadAxios";
-
-const unstarredButtonClick = (id: number, location: string) => {
-    Swal.fire({
-        title: '<p id="swalConfirmTitle">즐겨찾기에서 삭제하시겠습니까?</p>',
-        html: '<p id="swalConfirmText">알림 설정한 자리 삭제 시 알림도 함께 취소됩니다.<p>',
-        showCancelButton: true,
-        confirmButtonText: '삭제',
-        cancelButtonText: '취소',
-        confirmButtonColor: '#B90E0A',
-        cancelButtonColor: '#20633F',
-        heightAuto: false
-    }).then((res) => {
-        if (res.isConfirmed) {
-            nomadAxios.delete(`member/favorite/${id}`)
-            .then(() => {
-                const line = document.getElementById(`${location}`) as HTMLDivElement;
-                if (line)
-                    line.style.display = 'none';
-            })
-        }
-    });
-} 
+import unstarredButtonClick from "../logics/unstarredButtonClick";
 
 function StarredLine({id, location}: MyPageStarredData) {
     return (
