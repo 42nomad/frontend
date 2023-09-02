078a59783a941a3091f3176eef211c64e30a08b9
lamPolar <lovast7@gmail.com>
2023-09-02 15:09:23 +0900

    [RENAME] : meeting map Cx2->CX2 로 클러스터 이름 변경

--- src/pages/MeetingRoom/views/Cx2Map.tsx
+++ src/pages/MeetingRoom/views/Cx2Map.tsx
@@ -5,7 +5,7 @@ import AbleRoom from './AbleRoom';
 import GetMapInfo from '../logics/GetMapInfo';
 
 function Cx2Map() {
-	const mapInfo = GetMapInfo('Cx2');
+	const mapInfo = GetMapInfo('CX2');
 
 	return (
 		<div id="MeetingMap" className="grid grid-cols-6 grid-rows-6 h-2/3 w-3/4 mt-5" css={mapBorder}>
@@ -13,7 +13,7 @@ function Cx2Map() {
 				<DisableRoom roomName="좌석" />
 			</div>
 			<div className="col-span-3 row-span-1">
-				<AbleRoom mapInfo={mapInfo} cluster="Cx2" roomName="다각형 책상 A" />
+				<AbleRoom mapInfo={mapInfo} cluster="CX2" roomName="다각형 책상 A" />
 			</div>
 			<div className="col-span-1 row-span-1">
 				<DisableRoom roomName="좌석" />
@@ -22,19 +22,19 @@ function Cx2Map() {
 				<DisableRoom roomName="사물함" />
 			</div>
 			<div className="row-span-1 col-span-5">
-				<DisableRoom roomName="Cx2 Cluster" />
+				<DisableRoom roomName="CX2 Cluster" />
 			</div>
 			<div className="col-span-5 row-span-2">
-				<AbleRoom mapInfo={mapInfo} cluster="Cx2" roomName="오아시스" />
+				<AbleRoom mapInfo={mapInfo} cluster="CX2" roomName="오아시스" />
 			</div>
 			<div className="row-span-1 col-span-5">
-				<DisableRoom roomName="Cx2 Cluster" />
+				<DisableRoom roomName="CX2 Cluster" />
 			</div>
 			<div className="col-span-1 row-span-1">
 				<DisableRoom roomName="좌석" />
 			</div>
 			<div className="col-span-3">
-				<AbleRoom mapInfo={mapInfo} cluster="Cx2" roomName="다각형 책상 A" />
+				<AbleRoom mapInfo={mapInfo} cluster="CX2" roomName="다각형 책상 A" />
 			</div>
 			<div className="col-span-1 row-span-1">
 				<DisableRoom roomName="좌석" />
