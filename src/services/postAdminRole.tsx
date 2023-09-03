a67957a36ba90d541187dcf263a98c4df6527dae
lamPolar <lovast7@gmail.com>
2023-09-03 20:53:04 +0900

    [FIX] : api-getAdminRole params 수정

--- src/services/postAdminRole.tsx
+++ src/services/postAdminRole.tsx
@@ -1,6 +1,5 @@
 import nomadAxios from '../utils/nomadAxios';
 
-const postAdminRole = (intra: string, role: number) =>
-	nomadAxios.post('admin/role', null, { params: { intra: { intra }, role: { role } } });
+const postAdminRole = (intra: string, role: number) => nomadAxios.post('admin/role', null, { params: { intra, role } });
 
 export default postAdminRole;
