0385530bacef3d7cec33844eb5d8cf53f5e12396
lamPolar <lovast7@gmail.com>
2023-09-03 23:10:11 +0900

    [FIX] : api 에러 처리 추가

--- src/pages/MeetingRoom/logics/GetMapInfo.tsx
+++ src/pages/MeetingRoom/logics/GetMapInfo.tsx
@@ -6,9 +6,13 @@ function GetMapInfo(cluster: string) {
 	const [mapInfo, setMapInfo] = useState<RoomData[]>([]);
 
 	useEffect(() => {
-		getMeeting(cluster).then((res) => {
-			setMapInfo(res.data);
-		});
+		getMeeting(cluster)
+			.then((res) => {
+				setMapInfo(res.data);
+			})
+			.catch((err) => {
+				if (err.response.status === 404) swalAlert('회의실이 존재하지 않는 클러스터입니다');
+			});
 	}, [cluster]);
 	return mapInfo;
 }
