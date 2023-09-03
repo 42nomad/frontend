804a2244e20f874f828f7bfee617388e1db4f2b8
lamPolar <lovast7@gmail.com>
2023-09-03 20:26:26 +0900

    [REFACTOR] : mapNav left, right click 병합

--- src/pages/Cluster/views/Cluster.tsx
+++ src/pages/Cluster/views/Cluster.tsx
@@ -1,28 +1,26 @@
 import React, { useState } from 'react';
-import MapNav from '../../../components/MapNav/MapNav';
+import MapNav from '../../../components/MapNav/views/MapNav';
 import ClusterMap from './ClusterMap';
 import FloorMap from './FloorMap';
 import useDensity from '../logics/useDensity';
-import clusterLeftClick from '../logics/clusterLeftClick';
-import clusterRightClick from '../logics/clusterRightClick';
 import FixedButton from './FixedButton';
 
 function Cluster() {
-	const [ cluster, setCluster ] = useState(0);
+	const [cluster, setCluster] = useState(0);
 	const clusterNames = ['클러스터 활성도', 'C1', 'C2', 'CX1', 'CX2', 'C3', 'C4', 'C5', 'C6'];
-    const density = useDensity();
+	const density = useDensity();
 
 	return (
 		<div className="bg-nomad-sand h-full flex flex-col justify-center items-center pt-5 relative">
-			<MapNav
-				mapName={clusterNames[cluster]}
-				idx={cluster}
-				setState={setCluster}
-				leftButtonClick={clusterLeftClick}
-				rightButtonClick={clusterRightClick}
-			/>
-			{cluster ? <><ClusterMap cluster={cluster} mapName={clusterNames[cluster]} /><FixedButton setCluster={setCluster}/></>
-				: <FloorMap setCluster={setCluster} density={density} />}
+			<MapNav mapName={clusterNames[cluster]} idx={cluster} setIdx={setCluster} maxIdx={8} />
+			{cluster ? (
+				<>
+					<ClusterMap cluster={cluster} mapName={clusterNames[cluster]} />
+					<FixedButton setCluster={setCluster} />
+				</>
+			) : (
+				<FloorMap setCluster={setCluster} density={density} />
+			)}
 		</div>
 	);
 }
--- src/pages/MeetingRoom/views/MeetingRoom.tsx
+++ src/pages/MeetingRoom/views/MeetingRoom.tsx
@@ -1,36 +1,23 @@
 import React, { useState } from 'react';
-import { contentsCenter } from './MapStyle';
-import MapNav from '../../../components/MapNav/MapNav';
+import Header from '../../../components/Header/Header';
+import MapNav from '../../../components/MapNav/views/MapNav';
 import C3Map from './C3Map';
 import C1Map from './C1Map';
 import Cx2Map from './Cx2Map';
 import C5Map from './C5Map';
-
-const meetingLeftClick = (idx: number, setIdx: React.Dispatch<React.SetStateAction<number>>) => {
-	if (idx === 0) setIdx(3);
-	else setIdx(idx - 1);
-};
-
-const meetingRightClick = (idx: number, setIdx: React.Dispatch<React.SetStateAction<number>>) => {
-	if (idx === 3) setIdx(0);
-	else setIdx(idx + 1);
-};
 
 function MeetingRoom() {
 	const [idx, setIdx] = useState(0);
 	const clusters = ['C1', 'CX2', 'C3', 'C5'];
 	const meetingMaps = [<C1Map />, <Cx2Map />, <C3Map />, <C5Map />];
 	return (
-		<div className="bg-nomad-sand h-full flex-col" css={contentsCenter}>
-			<MapNav
-				mapName={clusters[idx]}
-				idx={idx}
-				setState={setIdx}
-				leftButtonClick={meetingLeftClick}
-				rightButtonClick={meetingRightClick}
-			/>
-			{meetingMaps[idx]}
-		</div>
+		<>
+			<Header />
+			<div className="bg-nomad-sand h-full flex-col flex items-center justify-center">
+				<MapNav mapName={clusters[idx]} idx={idx} setIdx={setIdx} maxIdx={3} />
+				{meetingMaps[idx]}
+			</div>
+		</>
 	);
 }
 
