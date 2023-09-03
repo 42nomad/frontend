a89ef5a4929fc09c26206580cd2e3a9b63daf66e
zoovely <yon041128@naver.com>
2023-09-03 19:00:12 +0900

    [FIX] : resolve #57 - 403, 401 처리 추가

--- src/utils/errorHandler.tsx
+++ src/utils/errorHandler.tsx
@@ -8,8 +8,20 @@ const errorHandler = (error: AxiosResponse) => {
             if (res.isConfirmed)
                 window.location.href = '/';
         });
+    else if (error.status === 403)
+        swalAlertTitle('페이지 접근 권한이 없습니다.', '이전 페이지로 이동합니다.', '확인', '#20633F')
+        .then((res) => {
+            if (res.isConfirmed)
+                window.history.back();
+        });
+    else if (error.status === 401)
+        swalAlertTitle('로그인 정보가 없습니다.', '로그인 페이지로 이동합니다.', '확인', '#20633F')
+        .then((res) => {
+            if (res.isConfirmed)
+                window.location.href = '/';
+        });
     else if (error.status === 429)
-        swalAlertTitle('요청이 너무 많습니다.', '잠시 후 다시 시도해주세요.', '확인', '#20633F')
+        swalAlertTitle('요청이 너무 많습니다.', '잠시 후 다시 시도해주세요.', '확인', '#20633F');
 }
 
 export default errorHandler;
